2c47dd98e99fd62368cd3d58ad6eb1dcc4122cc1
Rosangela22000127 <109716297+Rosangela22000127@users.noreply.github.com>
2024-05-24 07:29:20 +0000

    Agregando menu

--- pj3/Gramatica.java
+++ pj3/Gramatica.java
@@ -1,23 +1,35 @@
+import java.util.*;
 import java.io.*;
 
 public class Gramatica {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader archicvo = new BufferedReader(new FileReader(path));
+	public static void main(String[] args) {
+		//En el caso que escriban las banderas con mas argumentos de los que ncesitamos
+		if (args.length < 3) {
+			System.out.println("Uso: java Main 'archivo_gramatica' -afn o -afd 'archivo_salida'");
+			System.out.println("Uso: java Main 'archivo_gramatica' -check");
+			System.exit(1);
+		}
 
+		String archivo = args[0];
+		String opcion = args[1];
+		String salida = args[2];
+
+		AFN afn = new AFN(archivo);
+
+		//La ejecucion del lo pedido por el programa
+		if (opcion.equals("-afn")) {
+			// boolean aceptada = afn.accept();
+í
+		} else if (opcion.equals("-afd")) {
+			afn.toAFD(salida);
+		} else if (opcion.equals("-check")) {
+			
+		} else {
+			System.out.println("Revise el formato de ingreso: ");
+			System.out.println("Uso: java Main 'archivo_gramatica' -afn o -afd 'archivo_salida'");
+			System.out.println("Uso: java Main 'archivo_gramatica' -check");
+			System.exit(1);
+		}
 	}
-	/*
-	 * private static void printMenu() {
-	 * System.out.println("    Menu de ayuda   ");
-	 * System.out.println("------------------------");
-	 * System.out.println("Para ejecutar cualquier parte de este proyecto: ");
-	 * System.out.println("-basics <ruta de archivo>");
-	 * System.out.println("-rotate <ruta de archivo>");
-	 * System.out.println("-resize <ruta de archivo>");
-	 * System.out.println("-all <ruta de archivo>");
-	 * System.out.println("-help");
-	 * 
-	 * }
-	 */
-
 }
